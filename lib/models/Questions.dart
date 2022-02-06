47acd4d4d240ce41ee4056ecfa53d8a6324a0c5f
abdulazizmalikov18 <abdulazizmalikov98@gmail.com>
2022-02-06 11:57:55 +0500

    First Upload

---
+++ lib/models/Questions.dart
@@ -0,0 +1,71 @@
+class Question {
+  final int? id, answer;
+  final String? question;
+  final List<String>? options;
+
+  Question({this.id, this.question, this.answer, this.options});
+}
+
+const List sample_data = [
+  {
+    "id": 1,
+    "question":
+        "Flutter is an open-source UI software development kit created by ______",
+    "options": ['Apple', 'Google', 'Facebook', 'Microsoft'],
+    "answer_index": 1,
+  },
+  {
+    "id": 2,
+    "question": "When google release Flutter.",
+    "options": ['Jun 2017', 'Jun 2017', 'May 2017', 'May 2018'],
+    "answer_index": 2,
+  },
+  {
+    "id": 3,
+    "question": "What is Flutter ?",
+    "options": ['Android Development Kit', 'IOS Development Kit', 'Web Development Kit', 'SDK to build app'],
+    "answer_index": 3,
+  },
+  {
+    "id": 4,
+    "question": "Which programing language is used by Flutter ?",
+    "options": ['Java', 'Dart', 'Kotlin', 'Ruby'],
+    "answer_index": 1,
+  },
+  {
+    "id": 5,
+    "question": "Who created Dart programing language ?",
+    "options": ['Lars Bak & Kasper Lund', 'Brendan Eich', 'Bjarne Stroustrup', 'Jeremy Ashkenas'],
+    "answer_index": 0,
+  },
+  {
+    "id": 6,
+    "question": "Flutter is mainly optimized for 2D mobile apps that can run on ?",
+    "options": ['Android', 'iOS', 'Both 1 and 2', 'None of the above'],
+    "answer_index": 2,
+  },
+  {
+    "id": 7,
+    "question": "A widget that allows us to refresh the screen is called a ______",
+    "options": ['Stateless widgets', 'Stateful widget', 'Statebuild widget', 'All of the above'],
+    "answer_index": 1,
+  },
+  {
+    "id": 8,
+    "question": "The examples of the Stateless widget are ?",
+    "options": ['Text', 'Row', 'Column', 'All of the above'],
+    "answer_index": 3,
+  },
+  {
+    "id": 9,
+    "question": "pubspec.yaml file does not contain ?",
+    "options": ['Project general settings', 'Project dependencies', 'Project language', 'Project assets'],
+    "answer_index": 2,
+  },
+  {
+    "id": 10,
+    "question": "Which of the following are the advantages of Flutter ?",
+    "options": ['Cross-platform Development', 'Faster Development', 'Minimal code', 'All of the above'],
+    "answer_index": 3,
+  },
+];
---
+++ lib/screens/welcome/welcome_screen.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:quiz_app/constants.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:quiz_app/screens/quiz/quiz_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset("assets/icons/bg.svg", fit: BoxFit.fill,),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 2,),
+                  Text("Let's play Quiz", 
+                    style: TextStyle(
+                      fontSize: 40, 
+                      color: Colors.white
+                    )
+                  ),
+                  Text("Enter your information below"),
+                  Padding(
+                    padding: const EdgeInsets.only(top: kDefaultPadding),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Color(0xFF1C2341),
+                        hintText: "Full name",
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(12))
+                        )
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () => Get.to(QuizScreen()),
+                    child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(top: kDefaultPadding / 2),
+                      padding: EdgeInsets.all(kDefaultPadding * .75),
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient, 
+                        borderRadius: BorderRadius.all(Radius.circular(12))
+                      ),
+                      child: Text("Start a quiz", 
+                        style: Theme.of(context)
+                          .textTheme
+                          .headline6!
+                          .copyWith(color: kBlackColor)
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2,)
+                ],
+              ),
+            )
+          )
+        ],        
+      )
+    );
+  }
+}
